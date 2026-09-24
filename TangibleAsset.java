da82177ce8a2efc5391ddd014cc6181e447ccda7
clearworkbase-art <clearwork.base@gmail.com>
2026-09-24 20:17:14 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,14 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  privateString color;
+  public TangibleAsset(String name,int price, String Color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  public String getName(){tetum this.name;}
+  public int getPrice(){tetum this.price;}
+  public String getColor(){tetum this.Color;}
+
+}
